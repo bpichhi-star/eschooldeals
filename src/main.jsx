468f4354f7b23f9dd893c2eb59d32f58266f2b1c
bpichhi-star <bpichhi@gmail.com>
2026-03-22 12:08:08 -0700

    setup vite react project structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import EschoolDeals from "./EschoolDeals";
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode><EschoolDeals /></React.StrictMode>React.StrictMode>
-  );</React.StrictMode>
+  <React.StrictMode>
+    <EschoolDeals />
+  </React.StrictMode>
+);
