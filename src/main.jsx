a23c5c10c55470d2cee6c9b357b6823fa099c390
bpichhi-star <bpichhi@gmail.com>
2026-03-22 10:04:57 -0700

    Add main.jsx to render EschoolDeals component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import EschoolDeals from './EschoolDeals.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <EschoolDeals />
+  </StrictMode>,
+)
