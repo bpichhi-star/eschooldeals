002a7dcaa28eb883c0391fd2b9ac135230ff27f3
bpichhi-star <bpichhi@gmail.com>
2026-04-22 10:53:44 -0700

    Implement GET endpoint for homepage deals

--- app/api/deals/route.js
+++ app/api/deals/route.js
@@ -1,1 +1,6 @@
-// TODO
+import { getHomepageDeals } from '@/lib/queries/getHomepageDeals'
+
+export async function GET() {
+  const deals = await getHomepageDeals()
+  return Response.json({ deals })
+}
